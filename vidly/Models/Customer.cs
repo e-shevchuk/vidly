daafb3deff9397db563286c106a7b2d48f82f04b
Eugene <eugene@ES-Mac.local>
2021-07-06 08:51:43 +0300

    Added Customer and Movie model

---
+++ vidly/Models/Customer.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace vidly.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ vidly/Models/Movie.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace vidly.Models
+{
+    public class Movie
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+    }
+}
