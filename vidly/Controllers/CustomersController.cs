8797296aca0601e74dbb327060086dc57df43c49
Eugene Shevchuk <evgeniy.shevchuk@gmail.com>
2021-07-06 18:02:09 +0300

    Modified CustomersController to pull data from the DB

--- vidly/Controllers/CustomersController.cs
+++ vidly/Controllers/CustomersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using vidly.Data;
 using vidly.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -11,34 +12,34 @@ namespace vidly.Controllers
 {
     public class CustomersController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public CustomersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+        
         // GET: /<controller>/
         public IActionResult Index()
         {
-            var customers = GetCustomers();
+            var customers = _context.Customers.ToList();
 
             return View(customers);
         }
 
         public ActionResult Details(int id)
         {
-            var customers = GetCustomers();
-
-            var customer = customers.SingleOrDefault(m => m.Id == id);
+            var customer = _context.Customers.SingleOrDefault(m => m.Id == id);
 
             if (customer == null)
                 return NotFound();
 
             return View(customer);
-        }
-
-        // Hardcoded method for getting movies list
-        private static IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer>
-            {
-                new Customer { Id = 1, Name = "Rick" },
-                new Customer { Id = 2, Name = "Morty" }
-            };
         }
 
     }
